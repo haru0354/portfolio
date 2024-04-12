f964c33e10e894d9fddb06aabd42535ba3cda21e
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-04-12 22:22:03 +0900

    skillコンポーネントの作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Main2ColumnImage from "./components/portfolio/Work";
+import Skill from "./components/portfolio/Skill";
 
 export default function Home() {
   return (
     <>
       <Main2ColumnImage />
+      <Skill />
     </>
   );
 }
