36b85943b309bd89989d5d9ba423c65274105577
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-05-06 23:14:37 +0900

    300pxスクロールしたらtopへ戻るボタンが表示されるよう変更

--- app/components/ui/BackToTopButton .tsx
+++ app/components/ui/BackToTopButton .tsx
@@ -1,16 +1,43 @@
 "use client"
 
+import { useEffect, useState } from "react";
 import Button from "./Button";
 
 const BackToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY  > 300) { // 300pxより下にスクロールしたらボタンを表示
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
   const backToTop = () => {
-    window.scrollTo({
+    window.scroll({
       top: 0,
       behavior: "smooth",
     });
   };
 
-  return <Button onClick={backToTop} >トップへ戻る</Button>;
+  return (
+    <>
+      {isVisible && (
+        <button onClick={backToTop} style={{ position: 'fixed', bottom: '20px', right: '20px' }}>
+          トップへ戻る
+        </button>
+      )}
+    </>
+  );
 };
 
 export default BackToTopButton;
