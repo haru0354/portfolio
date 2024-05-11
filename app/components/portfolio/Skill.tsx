f71e5c35f21fbcd42dd6ea93df324368944bbd49
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-05-11 17:49:35 +0900

    skillのアニメーションの変更

--- app/components/portfolio/Skill.tsx
+++ app/components/portfolio/Skill.tsx
@@ -7,12 +7,12 @@ const Skill = () => {
         <h2 className="block w-full text-center text-3xl font-bold my-10">
           skill
         </h2>
-        <AnimatedItem
-          animation="fadeInDown"
-          elementType="div"
-          className="flex flex-wrap"
-        >
-          <div className="mr-2 my-2 p-4 w-[500px] border border-gray-400 rounded">
+        <div className="flex flex-wrap">
+          <AnimatedItem
+            animation="fadeInDown"
+            elementType="div"
+            className="mr-2 my-2 p-4 w-[500px] border border-gray-400 rounded"
+          >
             <h3 className="text-center border-b border-dashed pb-2 border-gray-500">
               自己開発で使用した技術
             </h3>
@@ -25,8 +25,12 @@ const Skill = () => {
               <li className="mb-1">Next.js</li>
               <li className="mb-1">Node.js</li>
             </ul>
-          </div>
-          <div className="ml-2 my-2 p-4 w-[500px] border border-gray-400 rounded">
+          </AnimatedItem>
+          <AnimatedItem
+            animation="fadeInDown"
+            elementType="div"
+            className="ml-2 my-2 p-4 w-[500px] border border-gray-400 rounded"
+          >
             <h3 className="text-center border-b border-dashed pb-2 border-gray-500">
               基礎を学習した技術(未開発)
             </h3>
@@ -39,8 +43,12 @@ const Skill = () => {
             <p className="mx-4 mb-4">
               「基礎を学習しても自己開発時に色々と調べたりすることも多かったです。そのため、あまり自身のない技術の一覧でもあります。」
             </p>
-          </div>
-          <div className="mr-2 my-2 p-4 w-[500px] border border-gray-400 rounded">
+          </AnimatedItem>
+          <AnimatedItem
+            animation="fadeInDown"
+            elementType="div"
+            className="mr-2 my-2 p-4 w-[500px] border border-gray-400 rounded"
+          >
             <h3 className="text-center border-b border-dashed pb-2 border-gray-500">
               その他
             </h3>
@@ -49,20 +57,22 @@ const Skill = () => {
               <li className="mb-1">Docker</li>
               <li className="mb-1">seo</li>
             </ul>
-          </div>
-          <div className="ml-2 my-2 p-4 w-[500px] border border-gray-400 rounded">
+          </AnimatedItem>
+          <AnimatedItem
+            animation="fadeInDown"
+            elementType="div"
+            className="ml-2 my-2 p-4 w-[500px] border border-gray-400 rounded"
+          >
             <h3 className="text-center border-b border-dashed pb-2 border-gray-500">
               学習方法・備考
             </h3>
-            <ul className="mx-4 my-8">
+            <ol className="list-decimal mx-8 my-8">
               <li className="mb-1">Udemyで基本をインプット</li>
               <li className="mb-1">実際にアプリを開発</li>
-              <li className="mb-1">
-                随時必要になる部分を日本語や英語で検索・ドキュメントの確認
-              </li>
-            </ul>
-          </div>
-        </AnimatedItem>
+              <li className="mb-1">日本語や英語で検索・ドキュメントの確認</li>
+            </ol>
+          </AnimatedItem>
+        </div>
       </div>
     </section>
   );
