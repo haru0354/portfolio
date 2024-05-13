c766a124c4fe0788f90c63fca99f1118976b8d6c
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-05-13 14:33:53 +0900

    aboutセクションのコンテンツの挿入

--- app/components/portfolio/About.tsx
+++ app/components/portfolio/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <section id="about" className="bg-gray-50 pt-10 pb-20 w-full">
       <div className="max-w-[1050px] mx-auto py-4 mt-10">
         <h2 className="block w-full text-3xl text-center font-bold my-10 ">
-          About This site
+          30代エンジニアのポートフォリオサイト
         </h2>
         <AnimatedItem
           elementType="div"
@@ -13,7 +13,15 @@ const About = () => {
           className="px-4 sm:px-0 w-full flex flex-col sm:flex-row"
         >
           <div className="w-full sm:flex-1 order-2 sm:order-1">
-            About This site,About This site,About This site
+            <p>このサイトは36歳エンジニア未経験のポートフォリオサイトです。</p>
+            <p>高校3年生の時に「ホームページビルダー」や「無料ブログ
+            サービス」を利用しつつ、約1年間の間、htmlとcssを少し勉強しながら収益を得ていました。その後、関係ない職種に就職。</p>
+            <p>28歳の時に「wordPress」などを利用しwebメディアを作成し、web広告業として個人事業主として開業をし、生計を立てて今に至ります。
+            36歳の時にエンジニアの職種に就きたいと決意し、勉強を開始しています。</p>
+            <ul className="my-4">
+              <li>GitHub: <a href="https://github.com/haru0354" target="blank" className="text-blue-500">https://github.com/haru0354</a></li>
+              <li></li>
+            </ul>
           </div>
           <div className="m-auto mb-8 order-1 sm:order-2">
             <img src="/engineer-image.png" alt="エンジニアの仕事道具" />
