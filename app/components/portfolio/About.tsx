a611e6b0bff527c06deb657cb93daadd4077d8a9
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-05-13 11:44:53 +0900

    aboutセクションのスマホデザインを追加

--- app/components/portfolio/About.tsx
+++ app/components/portfolio/About.tsx
@@ -3,14 +3,14 @@ import AnimatedItem from "../lib/AnimatedItem";
 const About = () => {
   return (
     <section id="about" className="bg-gray-50 pt-10 pb-20 w-full">
-      <div className="w-[1050px] mx-auto mt-20">
+      <div className="max-w-[1050px] mx-auto p-4 mt-10">
         <h2 className="block w-full text-3xl text-center font-bold my-10 ">
           About This site
         </h2>
         <AnimatedItem
           elementType="div"
           animation="fadeInDown"
-          className="mx-40 px-4 "
+          className="px-4 sm:px-20"
         >
           About This site,About This site,About This site
         </AnimatedItem>
