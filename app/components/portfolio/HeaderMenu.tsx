1d5a07e6b8f0d5c37548b69a549eb76fd91f4030
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-06-25 15:14:46 +0900

    ヘッダーメニューのスクロール部分の修正

--- app/components/portfolio/HeaderMenu.tsx
+++ app/components/portfolio/HeaderMenu.tsx
@@ -19,34 +19,33 @@ const HeaderMenu: React.FC<HeaderMenuProps> = ({
 
   return (
     <nav>
-      <ul className={`${ulClass}`}>
-        <a className="cursor-pointer" onClick={() => scrollToSection("about")}>
-          <li
-            className={`${liClass} p-4 hover:bg-sky-400 hover:text-white`}
-          >
-            About
-          </li>
-        </a>
-        <a onClick={() => scrollToSection("works")} className="cursor-pointer">
-          <li className={`${liClass} p-4 hover:bg-sky-400 hover:text-white`}>
-            Works
-          </li>
-        </a>
-        <a onClick={() => scrollToSection("skill")} className="cursor-pointer">
-          <li className={`${liClass} p-4 hover:bg-sky-400 hover:text-white`}>
-            Skills
-          </li>
-        </a>
-        <a
-          onClick={() => scrollToSection("profile")}
-          className="cursor-pointer"
-        >
-          <li className={`${liClass} p-4 hover:bg-sky-400 hover:text-white`}>
-            Profile
-          </li>
-        </a>
-      </ul>
-    </nav>
+    <ul className={`${ulClass}`}>
+      <li
+        className={`${liClass} p-4 hover:bg-sky-400 hover:text-white cursor-pointer`}
+        onClick={() => scrollToSection("about")}
+      >
+        <span>About</span>
+      </li>
+      <li
+        className={`${liClass} p-4 hover:bg-sky-400 hover:text-white cursor-pointer`}
+        onClick={() => scrollToSection("works")}
+      >
+        <span>Works</span>
+      </li>
+      <li
+        className={`${liClass} p-4 hover:bg-sky-400 hover:text-white cursor-pointer`}
+        onClick={() => scrollToSection("skill")}
+      >
+        <span>Skills</span>
+      </li>
+      <li
+        className={`${liClass} p-4 hover:bg-sky-400 hover:text-white cursor-pointer`}
+        onClick={() => scrollToSection("profile")}
+      >
+        <span>Profile</span>
+      </li>
+    </ul>
+  </nav>
   );
 };
 
