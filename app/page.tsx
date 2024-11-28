7760dade994e52ab722ab6bcb6ac78526134c8bd
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-11-28 12:27:29 +0900

    Skillをsectionフォルダに移動

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import About from "./components/section/About";
 import Work from "./components/portfolio/Work";
-import Skill from "./components/portfolio/Skill";
+import Skill from "./components/section/Skill";
 import Profile from "./components/section/Profile";
 import BackToTopButton from "./components/ui/BackToTopButton";
 import Footer from "./components/Footer";
