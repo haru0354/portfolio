ecab7d68b2600e5af0b0daecffff290bb03cb0ed
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-11-28 11:57:36 +0900

    BackToTopButton .tsxになっていたので名称を修正

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import About from "./components/portfolio/About";
 import Work from "./components/portfolio/Work";
 import Skill from "./components/portfolio/Skill";
 import Profile from "./components/portfolio/Profile";
-import BackToTopButton from "./components/ui/BackToTopButton ";
+import BackToTopButton from "./components/ui/BackToTopButton";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
