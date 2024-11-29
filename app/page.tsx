b9511c3b39877d5e7fad35c1e502cdfd2a6558a6
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-11-29 12:15:19 +0900

    Footerをlayoutフォルダへ移動

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import About from "./components/section/About";
 import Work from "./components/work/Work";
 import Skill from "./components/section/Skill";
 import Profile from "./components/section/Profile";
-import Footer from "./components/Footer";
+import Footer from "./components/layout/Footer";
 import BackToTopButton from "./components/ui/BackToTopButton";
 
 export default function Home() {
