a6886fce0162a92be7dddf11c34d7c27992933d3
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-06-21 11:34:53 +0900

    2ページ目の予定はないが対応できるようlayoutに記述を変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,17 @@
-import Header from "./components/layout/Header";
 import About from "./components/section/About";
+import SkillImage from "./components/section/SkillImage";
 import Work from "./components/work/Work";
 import Skill from "./components/section/Skill";
 import Profile from "./components/section/Profile";
-import Footer from "./components/layout/Footer";
-import BackToTopButton from "./components/ui/BackToTopButton";
-import SkillImage from "./components/section/SkillImage";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main>
-        <About />
-        <SkillImage />
-        <Work />
-        <Skill />
-        <Profile />
-        <BackToTopButton />
-      </main>
-      <Footer />
-    </>
+    <main>
+      <About />
+      <SkillImage />
+      <Work />
+      <Skill />
+      <Profile />
+    </main>
   );
 }
