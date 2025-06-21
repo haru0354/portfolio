fd3c13c5e831cdd9531033b8f93e42a19f6e4df1
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-06-21 11:27:49 +0900

    SkillImageコンポーネントの作成

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Skill from "./components/section/Skill";
 import Profile from "./components/section/Profile";
 import Footer from "./components/layout/Footer";
 import BackToTopButton from "./components/ui/BackToTopButton";
+import SkillImage from "./components/section/SkillImage";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Header />
       <main>
         <About />
+        <SkillImage />
         <Work />
         <Skill />
         <Profile />
