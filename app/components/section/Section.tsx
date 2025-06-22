79f146be5296552765c585e291184b0c4d7dce34
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-06-22 11:54:47 +0900

    Sectionコンポーネントのリファクタリング

--- app/components/section/Section.tsx
+++ app/components/section/Section.tsx
@@ -16,27 +16,18 @@ const Section: React.FC<SectionProps> = ({
   h1 = false,
 }) => {
   const bgColor = bgBlue ? "bg-blue-100" : "bg-white";
+  const elementType = h1 ? "h1" : "h2";
 
   return (
     <section id={id} className={`w-full pt-10 sm:pb-10 ${bgColor}`}>
       <div className="max-w-[1050px] mx-auto mt-10 px-2 py-4">
-        {h1 ? (
-          <AnimatedItem
-            animation="fadeInOpacity"
-            elementType="h1"
-            className="w-full text-3xl text-center font-bold my-10 leading-relaxed"
-          >
-            {title}
-          </AnimatedItem>
-        ) : (
-          <AnimatedItem
-            animation="fadeInOpacity"
-            elementType="h2"
-            className="w-full text-center text-3xl font-bold my-10 leading-relaxed"
-          >
-            {title}
-          </AnimatedItem>
-        )}
+        <AnimatedItem
+          animation="fadeInOpacity"
+          elementType={elementType}
+          className="w-full text-3xl text-center font-bold my-10 leading-relaxed"
+        >
+          {title}
+        </AnimatedItem>
         {children}
       </div>
     </section>
